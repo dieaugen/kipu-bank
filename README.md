2160d1243da9c85423ee57b1cd4fb538e090f848
dieaugen <100375910+dieaugen@users.noreply.github.com>
2025-10-05 16:13:45 -0300

    add: agrego Solidity contract

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # kipu-bank
+
---
+++ contracts/KipuBank.sol
@@ -0,0 +1,151 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+/**
+ * @title KipuBank 
+ * @dev KipuBank manager
+ */
+contract KipuBank {
+
+    // @notice Límite máximo de retiro por transacción
+    uint256 public immutable limitMaxRetiroWei;
+    // @notice Límite de total Eth en depósitos en el banoc    
+    uint256 public immutable bankCap;
+
+    //@notice Contador de Despositos totales realizadas por sus clientes
+    uint256 public countTotalDepositos;
+    
+    //@notice Contador de extacciones totales realizadas por sus clientes
+    uint256 public countTotalExtracciones;
+    
+    //@notice Almacena la liquidez actual del banco
+    uint256 public totalBancoBalanceWei; 
+
+    /// @notice  Informacion sobre las operaciones de cada usuario
+    struct UserInfo{
+        uint256 balance;
+        uint256 cantdepositos;
+        uint256 cantretiros;
+    }
+    mapping (address => UserInfo ) private _users;
+    
+
+   
+    // @notice ErrRetiroExcedeLimite error cuando el retiro se excede el límite permitido.
+    error ErrRetiroExcedeLimite(uint256 _monto, uint256 _limite);
+    
+    // @notice ErrBalanceUsuarioInsuficiente  cuando no hay suficiente en la cuenta.
+    error ErrBalanceUsuarioInsuficiente(uint256 _monto, uint256 _balance);
+    
+    // @notice ErrDepositoExcedeLimite error cuando se excede el maximo total permitido del banco (bankCap).
+    error ErrDepositoExcedeLimite(uint256 _monto, uint256 _totalactual, uint256 _maxbanco );
+    
+    // notice ErrMontoCero cuando el monto ingresado es igual a cero duh!!
+    error ErrMontoCero();
+    
+    // @notice TransferFailed error cuando la transferencia falla.
+    error TransferFailed();
+
+    
+    // Modificador.
+    // @notice Mientras el valor a depositar es mayor que cero
+    modifier montoValido() {
+        if (msg.value == 0) revert ErrMontoCero();
+        //require(msg.value > 0, "Monto invalido o igual a cero");
+        _;
+    }
+    
+
+
+    /**
+     * @notice Deposito relizado correctamente.
+     * @param _user addr del usuario.
+     * @param _monto   Monto depositado (wei).
+     * @param _balanceUsuario Balance total del Usuario.
+     * @param _balanceBanco Balance total del Banco.
+     */
+    event Deposito(address indexed _user, uint256 _monto, uint256 _balanceUsuario, uint256 _balanceBanco);
+
+     /**
+     * @notice Retiro exitoso por parte de un usuario.
+     * @param _user Dirección del usuario que retiró.
+     * @param _monto   Monto retirado en wei.
+     * @param _balanceUsuario Balance total del Usuario.
+     * @param _balanceBanco Balance total del Banco.
+     */
+    event Retiro(address indexed _user, uint256 _monto, uint256 _balanceUsuario, uint256 _balanceBanco);
+
+
+    /**
+    *   Inicializacion de la config del contrato.
+    */
+    constructor(uint256 _bankCap, uint256 _withdrawLimit) {
+        if ( _bankCap == 0 || _withdrawLimit == 0 || _withdrawLimit > _bankCap) {
+            //revert InvalidParams();
+        }
+        bankCap = _bankCap;
+        limitMaxRetiroWei = _withdrawLimit;
+    }
+
+    
+    /**
+     * @notice Deposita ETH en la bóveda del remitente.
+     */
+    function depositar() external payable montoValido {
+        uint256 _monto = msg.value; 
+        address _cuenta = msg.sender;
+        
+        if ( totalBancoBalanceWei + _monto > bankCap) {
+            revert ErrDepositoExcedeLimite(_monto, totalBancoBalanceWei, bankCap ); 
+        }
+        
+        //Balance por usuario:
+        _users[_cuenta].balance += _monto;
+        _users[_cuenta].cantdepositos ++;
+        
+        //Balance Banco:
+        totalBancoBalanceWei += _monto;
+        countTotalDepositos++;
+
+
+        emit Deposito(_cuenta, _monto, _users[_cuenta].balance, totalBancoBalanceWei);
+        
+    }
+
+
+    /**
+     * @notice Retira Weis de la bóveda del usuario, verifica límites por transacción.
+     */
+    function Retirar() external payable montoValido() { 
+        uint256 _monto = msg.value; 
+        address _cuenta = msg.sender;
+
+        if ( _monto > limitMaxRetiroWei) {
+            revert ErrRetiroExcedeLimite( _monto, limitMaxRetiroWei); 
+        }
+        // El usuario debe tener liquidez:
+         if (_users[ _cuenta ].balance < _monto) {
+            revert ErrBalanceUsuarioInsuficiente(_monto, _users[ _cuenta ].balance);
+        }
+        
+        _users[_cuenta].balance -= _monto;
+        _users[_cuenta].cantretiros ++;
+
+        totalBancoBalanceWei -= _monto;
+        countTotalExtracciones++;
+
+        // @TODO: hacer tranferrir atomico
+        //_transferirSeguro(payable(_addr), _monto);
+
+
+        emit Retiro(_cuenta, _monto, _users[_cuenta].balance, totalBancoBalanceWei);
+        
+
+    }
+
+    function getBalanceCuenta(address _cuenta) external view returns (uint256) {
+        return _users[_cuenta].balance;
+    }
+
+}
